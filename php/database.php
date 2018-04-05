070d136f44b645ef77cd2b820d14e3bfee306884
MythaelX <even.clement@live.fr>
2018-04-05 14:32:21 +0200

    modified:   php/database.php

--- php/database.php
+++ php/database.php
@@ -2,6 +2,7 @@
 <?php
 require_once("../PHPClass/bdd.php");
 require_once("../php/class.php");
+
 if($_SERVER["HTTP_HOST"] == "127.0.0.1"){
   $bdd = new Bdd("mysql", "127.0.0.1:3306", "user2", "user2", "user2");
 } else if($_SERVER["HTTP_HOST"] == "localhost"){
@@ -11,15 +12,8 @@ if($_SERVER["HTTP_HOST"] == "127.0.0.1"){
 }
 
 if (isset($_POST['libelle'])) {
-  $libelle=$_POST['libelle'];
-  $corde=$_POST['corde'];
-  $tmax=$_POST['tmax'];
-  $tmaxmm=($tmax/100)*$corde;
-  $fmax=$_POST['fmax'];
-  $fmaxmm=($fmax/100)*$corde;
-  $nb_points=$_POST['nb_points']
-  $interval=$_POST['interval']
-  bdd->insert("parametre", $val);
+  $parametre= new Para($_POST['libelle'],$_POST['corde'],$_POST['tmax'],(($tmax/100)*$corde),$_POST['fmax'],(($fmax/100)*$corde),=$_POST['nb_points'],$_POST['interval']);
+  //bdd->insert("parametre", $val);
 }
 
   ?>
