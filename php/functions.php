2bd460cd0c1277ed94c6fd1eac07fadb8195e98d
mathias <mathias.cabiochdelalande@gmail.com>
2018-04-05 15:08:09 +0200

    mise a jour automatique

--- php/functions.php
+++ php/functions.php
@@ -5,7 +5,7 @@
 		$points;
 		$index = 0;
 		$tprec = 0;
-		for($i = 0; $i < $param->nb_points; $i += $param->interval){
+		for($i = 0; $i < intval($param->nb_points); $i += intval($param->interval)){
 			$t = -1 * (1.015 * pow(($i/$param->corde), 4) - 2.843 * pow(($i/$param->corde), 3) + 3.516 * pow(($i/$param->corde), 2) + 1.26 * ($i/$param->corde) - 2.969 * sqrt($i/$param->corde)) * $param->tmaxmm;
 			$f = -4 * (pow(($i/$param->corde), 2) - ($i/$param->corde)) * $param->fmaxmm;
 			$yintra = $f + $t/2;
