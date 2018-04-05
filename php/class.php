cea0b8b135031d6a3d21d3068e2947d315c7722d
mathias <mathias.cabiochdelalande@gmail.com>
2018-04-05 14:30:31 +0200

    mise a jour automatique

--- php/class.php
+++ php/class.php
@@ -1,40 +1,41 @@
 <?php
-class Param(){
-    $libelle;
-    $corde;
-    $tmax;
-    $tmaxmm;
-    $fmax;
-    $fmaxmm;
-    $nb_points;
-    $interval;
-    function __construct($libelle,$corde,$tmax,$tmaxmm,$fmax,$fmaxmm,$nb_points,$interval){
-      $this->libelle=$libelle;
-      $this->corde=$corde;
-      $this->tmax=$tmax;
-      $this->tmaxmm=$tmaxmm;
-      $this->fmax=$fmax;
-      $this->fmaxmm=$fmaxmm;
-      $this->nb_points=$nb_points;
-      $this->interval=$interval;
-    }
-}
+	class Param {
+		public $libelle;
+		public $corde;
+		public $tmax;
+		public $tmaxmm;
+		public $fmax;
+		public $fmaxmm;
+		public $nb_points;
+		public $interval;
 
-class Cambrure(){
-  $x;
-  $t;
-  $f;
-  $yintra;
-  $yextra;
-  $igx;
-  function __construct($x,$t,$f,$yintra,$yextra,$igx){
-    $this->x=$x;
-    $this->t=$t;
-    $this->f=$f;
-    $this->yintra=$yintra;
-    $this->yextra=$yextra;
-    $this->$igx=$igx;
+		function __construct($libelle,$corde,$tmax,$tmaxmm,$fmax,$fmaxmm,$nb_points,$interval){
+			$this->libelle=$libelle;
+			$this->corde=$corde;
+			$this->tmax=$tmax;
+			$this->tmaxmm=$tmaxmm;
+			$this->fmax=$fmax;
+			$this->fmaxmm=$fmaxmm;
+			$this->nb_points=$nb_points;
+			$this->interval=$interval;
+		}
+	}
 
-  }
-}
+	class Cambrure{
+		public $x;
+		public $t;
+		public $f;
+		public $yintra;
+		public $yextra;
+		public $igx;
+		
+		function __construct($x,$t,$f,$yintra,$yextra,$igx){
+			$this->x=$x;
+			$this->t=$t;
+			$this->f=$f;
+			$this->yintra=$yintra;
+			$this->yextra=$yextra;
+			$this->igx=$igx;
+		}
+	}
 ?>
---
+++ php/functions.php
@@ -0,0 +1,9 @@
+<?php
+	require_once("class.php");
+	
+	function computePoints(param){
+		for($i = 0; $i < $param->nb_points; $i += $param->interval){
+			
+		}
+	}
+?>
