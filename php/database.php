f2a602ea44d0f47dc52efec16d0e05061f678ffc
MythaelX <even.clement@live.fr>
2018-04-05 13:51:31 +0200

    erreur bdd

--- php/database.php
+++ php/database.php
@@ -1,10 +1,14 @@
 
 <?php
 require_once("../PHPClass/bdd.php");
-ECHO "OKk";
-//$bdd = new Bdd("mysql","172.31.4.25:3306","user2","user2", "user2");
-$bdd= new PDO("mysql:host=172.31.4.25;dbname=user2;charset=UTF8;", "user2", "user2");
-ECHO "OK1";
+if($_SERVER["HTTP_HOST"] == "127.0.0.1"){
+  $bdd = new Bdd("mysql", "127.0.0.1:3306", "user2", "user2", "user2");
+} else if($_SERVER["HTTP_HOST"] == "localhost"){
+  $bdd = new Bdd("mysql", "localhost:3306", "user2", "user2", "user2");
+} else {
+  $bdd = new Bdd("mysql", "172.31.4.25:3306", "user2", "user2", "user2");
+}
+
 if (isset($_POST['libelle'])) {
   $libelle=$_POST['libelle'];
   $date=$_POST['date'];
