bb9dc69fe0f5180f91b9f34b108a41b0b10142e6
MythaelX <even.clement@live.fr>
2018-04-05 14:16:55 +0200

    bla bla

--- php/database.php
+++ php/database.php
@@ -1,6 +1,44 @@
 
 <?php
 require_once("../PHPClass/bdd.php");
+class Param(){
+    $libelle;
+    $corde;
+    $tmax;
+    $tmaxmm;
+    $fmax;
+    $fmaxmm;
+    $nb_points;
+    $interval;
+    function __construct($libelle,$corde,$tmax,$tmaxmm,$fmax,$fmaxmm,$nb_points,$interval){
+      $this->libelle=$libelle;
+      $this->corde=$corde;
+      $this->tmax=$tmax;
+      $this->tmaxmm=$tmaxmm;
+      $this->fmax=$fmax;
+      $this->fmaxmm=$fmaxmm;
+      $this->nb_points=$nb_points;
+      $this->interval=$interval;
+    }
+}
+
+class Cambrure(){
+  $x;
+  $t;
+  $f;
+  $yintra;
+  $yextra;
+  $igx;
+  function __construct($x,$t,$f,$yintra,$yextra,$igx){
+    $this->x=$x;
+    $this->t=$t;
+    $this->f=$f;
+    $this->yintra=$yintra;
+    $this->yextra=$yextra;
+    $this->$igx=$igx;
+
+  }
+}
 if($_SERVER["HTTP_HOST"] == "127.0.0.1"){
   $bdd = new Bdd("mysql", "127.0.0.1:3306", "user2", "user2", "user2");
 } else if($_SERVER["HTTP_HOST"] == "localhost"){
@@ -11,7 +49,6 @@ if($_SERVER["HTTP_HOST"] == "127.0.0.1"){
 
 if (isset($_POST['libelle'])) {
   $libelle=$_POST['libelle'];
-  $date=$_POST['date'];
   $corde=$_POST['corde'];
   $tmax=$_POST['tmax'];
   $tmaxmm=($tmax/100)*$corde;
