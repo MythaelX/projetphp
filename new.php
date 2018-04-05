e12eb1e54e5cd5b93dc1a7617192f11cc4b2826f
mathias <mathias.cabiochdelalande@gmail.com>
2018-04-05 11:55:38 +0200

    mise a jour automatique

--- new.php
+++ new.php
@@ -4,7 +4,7 @@
 
 	$head = new Head_Creator("./");
 
-	$head->head_title("Site de test");
+	$head->head_title("Chen-co.corp");
 	$head->head_charset("utf-8");
 
 	/* Styles du site */
@@ -36,33 +36,42 @@
 		<header><?php require_once("php/header.temp.inc"); ?></header>
 		<nav class="navbar navbar-default"><?php require_once("php/nav.temp.inc"); ?></nav>
 		<main>
-      <div class="container" >
-      <form method="post" action="php/database.php"/>
-        Label:<br/>
-        <input type="text" name="libelle"/>
-        <br/>
-        Date:<br/>
-        <input type="date" name="date"/>
-        <br/>
-        Corde:<br/>
-        <input type="number" name="corde"/>
-        <br/>
-        tmax %:<br/>
-        <input type="number" name="tmax"/>
-        <br/>
-        fmax %:<br/>
-        <input type="number" name="fmax"/>
-        <br/>
-        nb_points:<br/>
-        <input type="number" name="nb_points"/>
-        <br/>
-        interval entre les points:<br/>
-        <input type="number" name="interval"/>
-        <br/>
-        <input type="submit" value="envoyer"/>
-      </div>
-    </form>
-    </main>
+			<form method="post" class="container" action="php/database.php">
+				<div class="form-group">
+					<label for="libelle">Intitulé :</label><br />
+					<input type="text" class="form-control" name="libelle" />
+				</div>
+				<!--<label for="">Date:<br />
+				<input type="date" name="date"/>
+				<br />-->
+				<div class="form-group">
+					<label for="corde">Taille de la corde (en mm):<br />
+					<input type="number" class="form-control" name="corde"/>
+				</div>
+				<div class="form-group">
+					<div class="form-group" style="display: inline-block; width: 45%;">
+						<label for="tmax">Tmax (en %) :<br />
+						<input type="number" class="form-control" name="tmax" />
+					</div>
+					<div class="form-group" style="display: inline-block; width: 45%;">
+						<label for="fmax">Fmax (en %) :<br />
+						<input type="number" class="form-control" name="fmax" />
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="form-group" style="display: inline-block; width: 45%;">
+						<label for="nb_points">Nombre de points pour les calculs :<br />
+						<input type="number" class="form-control" name="nb_points" />
+					</div>
+					<div class="form-group" style="display: inline-block; width: 45%;">
+						<label for="interval">Interval entre les points:<br />
+						<input type="number" class="form-control" name="interval" />
+					</div>
+				</div>
+				<input type="submit" value="Envoyer" />
+				</div>
+			</form>
+		</main>
 		<footer><?php require_once("php/footer.temp.inc"); ?></footer>
 	</body>
 </html>
