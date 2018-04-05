17a19ac738af30102cb92eb754e01ad224e34dfa
MythaelX <even.clement@live.fr>
2018-04-05 14:53:40 +0200

    database.php

--- php/database.php
+++ php/database.php
@@ -1,7 +1,7 @@
 
 <?php
 require_once("../PHPClass/bdd.php");
-require_once("../php/class.php");
+require_once("../php/functions.php");
 
 if($_SERVER["HTTP_HOST"] == "127.0.0.1"){
   $bdd = new Bdd("mysql", "127.0.0.1:3306", "user2", "user2", "user2");
@@ -12,8 +12,9 @@ if($_SERVER["HTTP_HOST"] == "127.0.0.1"){
 }
 
 if (isset($_POST['libelle'])) {
-  $parametre= new Para($_POST['libelle'],$_POST['corde'],$_POST['tmax'],(($tmax/100)*$corde),$_POST['fmax'],(($fmax/100)*$corde),=$_POST['nb_points'],$_POST['interval']);
-  //bdd->insert("parametre", $val);
+  $parametre = new Param($_POST['libelle'],$_POST['corde'],$_POST['tmax'],(($tmax/100)*$corde),$_POST['fmax'],(($fmax/100)*$corde),$_POST['nb_points'],$_POST['interval']);
+  $points = computePoints($parametre);
+  $bdd->insert("parametre", "NULL,".$parametre->libelle.",".$parametre->corde.",".$parametre->tmax.",".$parametre->tmaxmm.",".$parametre->fmax.",".$parametre->fmaxmm.",".$parametre->nb_points.",".$parametre->interval);
 }
 
   ?>
