281c9f0ac8a6af3036de0bb1d1847dafe1cabffe
mathias <mathias.cabiochdelalande@gmail.com>
2018-04-10 16:46:30 +0200

    mise a jour automatique

--- php/database.php
+++ php/database.php
@@ -1,15 +1,7 @@
 <?php
+	require_once("functions.php");
 	require_once("../PHPClass/bdd.php");
-	require_once("../php/functions.php");
-	
-	/* Connexion à la base de données */
-	if($_SERVER["HTTP_HOST"] == "127.0.0.1"){
-		$bdd = new Bdd("mysql", "127.0.0.1:3306", "user2", "user2", "user2");
-	} else if($_SERVER["HTTP_HOST"] == "localhost"){
-		$bdd = new Bdd("mysql", "localhost:3306", "user2", "user2", "user2");
-	} else {
-		$bdd = new Bdd("mysql", "172.31.4.25:3306", "user2", "user2", "user2");
-	}
+	require_once("dbConnect.inc");
 
 	/* Traitement des données si la variable $_POST["libelle"], donc si des données ont été envoyées */
 	if (isset($_POST['libelle'])) {
@@ -29,11 +21,11 @@
 			}
 		} else if($_POST['action']=="update"){
 			/* Si c'est une modification */
-			$bdd->update("parametre", "libelle = '".$parametre->libelle."', corde = ".$parametre->corde.", tmax = ".$parametre->tmax.", tmaxmm = ".$parametre->tmaxmm.", fmax = ".$parametre->fmax.", fmaxmm = ".$parametre->fmaxmm.", nb_points = ".$parametre->nb_points, "WHERE id = ".$_POST['id_param']);
-			$bdd->delete("cambrure", "id_param = ".$_POST['id_param']);
+			$bdd->update("parametre", "libelle = '" . $parametre->libelle . "', corde = " . $parametre->corde . ", tmax = " . $parametre->tmax . ", tmaxmm = " . $parametre->tmaxmm . ", fmax = " . $parametre->fmax . ", fmaxmm = " . $parametre->fmaxmm . ", nb_points = " . $parametre->nb_points, "WHERE id = " . $_POST['id_param']);
+			$bdd->delete("cambrure", "id_param = " . $_POST['id_param']);
 			
 			for ($i=0; $i <sizeof($points); $i++) {
-				$bdd->insert("cambrure","NULL, ".$points[$i]->x.", ".$points[$i]->t.", ".$points[$i]->f.", ".$points[$i]->yintra.", ".$points[$i]->yextra.", ".$_POST['id_param'].", ".$points[$i]->igz);
+				$bdd->insert("cambrure", "NULL, " . $points[$i]->x . ", " . $points[$i]->t . ", " . $points[$i]->f . ", " . $points[$i]->yintra . ", " . $points[$i]->yextra . ", " . $_POST['id_param'] . ", " . $points[$i]->igz);
 			}
 		}
 	}
--- php/request.php
+++ php/request.php
@@ -2,17 +2,9 @@
 	session_start();
 	require_once("../PHPClass/getter.php");
 	require_once("../PHPClass/bdd.php");
+	require_once("dbConnect.inc");
 	
 	$res = nextRes();
-	
-	/* Connexion à la base de données */
-	if($_SERVER["HTTP_HOST"] == "127.0.0.1"){
-		$bdd = new Bdd("mysql", "127.0.0.1:3306", "user2", "user2", "user2");
-	} else if($_SERVER["HTTP_HOST"] == "localhost"){
-		$bdd = new Bdd("mysql", "localhost:3306", "user2", "user2", "user2");
-	} else {
-		$bdd = new Bdd("mysql", "172.31.4.25:3306", "user2", "user2", "user2");
-	}
 	
 	echo $res . "\n";
 	
