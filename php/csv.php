3ed841b4fdbc93bd899bb406110db328c0831fc9
MythaelX <even.clement@live.fr>
2018-04-11 15:49:08 +0200

    maj

--- php/csv.php
+++ php/csv.php
@@ -1,16 +1,27 @@
 <?php
+function createCSV($id){
+  require_once("../PHPClass/bdd.php");
+  require("dbConnect.inc");
+  $datasP = $bdd->select("parametre", "*", "WHERE id=".$id);
+  $datas = $bdd->select("cambrure", "*", "WHERE id_param=".$id);
 
-$list = array (
-   array('aaa', 'bbb', 'ccc', 'dddd'),
-   array('123', '456', '789'),
-   array('"aaa"', '"bbb"')
-);
+  $data_csv = array (
+    array('aaa', 'bbb', 'ccc', 'dddd'),
+    array('123', '456', '789'),
+    array('"aaa"', '"bbb"')
+  );
+  for($i = 0; $i < sizeOf($datas); ++$i){
+    //ajout de datas dans le tableau
+  }
 
-$fp = fopen('file.csv', 'w+');
+  // Create the filename
+  $csvfile = $datasP[0]["libelle"] . "_" . uniqid("", true) . ".csv";
+  $fp = fopen($csvfile, 'w+');
 
-foreach ($list as $fields) {
+  foreach ($data_csv as $fields) {
     fputcsv($fp, $fields);
-}
+  }
 
-fclose($fp);
+  fclose($fp);
+}
 ?>
